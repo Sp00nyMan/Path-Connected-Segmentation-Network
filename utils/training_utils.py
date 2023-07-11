f3db0dfbecc34341150a5dbd2e793d489f8dc90c
Sp00nyMan <mrstormproxdd@gmail.com>
2023-07-11 19:09:00 +0200

    Final experimentation

--- utils/training_utils.py
+++ utils/training_utils.py
@@ -18,16 +18,19 @@ def train(plain_model: Network, convex_model: Network, flow_model: FlowNetwork,
           epochs: int = 500, all_data_fraction: int = 0.8,
           save_log: bool = False, save_folder: str = r".snapshots"):
     
+    if flow_model is not None and convex_model is None:
+        raise ValueError("Training the flow model doesn't make sense without the convex model")
+
     if save_log:
         plain_cm = CheckpointManager(save_folder, "plain")
-        convex_cm = CheckpointManager(save_folder, "convex")
-        flow_cm = CheckpointManager(save_folder, "flow")
+        convex_cm = CheckpointManager(save_folder, "convex_apples_flow")
+        flow_cm = CheckpointManager(save_folder, "flow_apples")
 
-        training_lw = LogWriter(save_folder, "training")
+    # training_lw = LogWriter(save_folder, "mandarins_connected")
     
     with tqdm(range(epochs)) as progress:
         for epoch in progress:
-            if save_log: start_time = time()
+            start_time = time()
 
             for inputs, labels in data_loader:
                 random_indices = np.random.random_integers(0, len(all_data) - 1, 
@@ -48,9 +51,9 @@ def train(plain_model: Network, convex_model: Network, flow_model: FlowNetwork,
 
                 # TODO Change to MSELoss
                 if epoch > 200:
-                    loss += torch.mean((outputs_convex - (outputs > 0.5).float()) ** 2)
-                elif epoch > 30:
                     loss += torch.mean((outputs_convex - outputs) ** 2)
+                elif epoch > 30:
+                    loss += torch.mean((outputs_convex - (outputs > 0.5).float()) ** 2)
 
                 optimizer.zero_grad()
                 loss.backward()
@@ -60,9 +63,10 @@ def train(plain_model: Network, convex_model: Network, flow_model: FlowNetwork,
             
             if isinstance(scheduler, ReduceLROnPlateau):
                 scheduler.step(loss)
+            
+            # training_lw.log(epoch, start_time, loss, 0, 0)
 
             if save_log:
-                training_lw.log(epoch, start_time, loss, 0, 0)
                 progress.set_postfix(loss=float(loss))
                 if epoch % 50:
                     plain_cm.save(plain_model, optimizer, epoch, loss)
